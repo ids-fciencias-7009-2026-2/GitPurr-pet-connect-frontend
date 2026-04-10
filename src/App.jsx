47d0822c1ed5a69562adfa5d0980cdef61d72ed2
Karina Hernández <karina_hf3@hotmail.com>
2026-04-10 17:21:33 -0600

    feat: logout implementado

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,38 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./paginas/Login"
 import Home from "./paginas/Home"
 import Register from "./paginas/Register"
+import Profile from "./paginas/Profile"
+import Update from "./paginas/Update"
+import ProtectedRoute from "./componentes/ProtectedRoute"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+
+        {/* públicas */}
         <Route path="/login" element={<Login />}/>
         <Route path="/register" element={<Register />}/>
-        <Route path="/home" element={<Home />}/>
+
+        {/* protegidas */}
+        <Route path="/home" element={
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        }/>
+
+        <Route path="/profile" element={
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>
+        }/>
+
+        <Route path="/update" element={
+          <ProtectedRoute>
+            <Update />
+          </ProtectedRoute>
+        }/>
+
         <Route path="*" element={<Navigate to="/login" />}/>
       </Routes>
     </BrowserRouter>
