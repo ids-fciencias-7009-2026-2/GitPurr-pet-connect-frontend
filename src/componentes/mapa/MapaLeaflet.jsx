32bb694cb99d76f67dea05ca2860749c6cb69b59
AngelIsaacGarcia <angelisaac.garcia@ciencias.unam.mx>
2026-05-23 16:48:35 -0600

    refactor(mapa): reorganiza componentes y arquitectura del mapa

---
+++ src/componentes/mapa/MapaLeaflet.jsx
@@ -0,0 +1,48 @@
+import { MapContainer, Marker, TileLayer, Popup } from "react-leaflet";
+import { iconoUsuario, iconoGeneral, iconoAnimalito } from "../mapa/iconosMapa.jsx";
+import MarkerClusterGroup from "react-leaflet-cluster";
+import "leaflet/dist/leaflet.css";
+import "../../estilos/componentes/MapaLeaflet.css";
+
+function MapaLeaflet({ posicion, animalitos, animalitoHandler}) {
+    return (
+        <MapContainer className="mapa" center={posicion} zoom={15}>
+            {/** Trae la imagen que permite renderizar el mapa */}
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+
+            {/** Marcador del usuario */}
+            <Marker position={posicion} icon={iconoUsuario}>
+                <Popup>
+                    <div className="popup-usuario">
+                        <div className="popup-header popup-usuario-header"> ¡Aquí estás tú! </div>
+                    </div>
+                </Popup>
+            </Marker>
+
+            {/** Agrupa todos los animalitos cercanos en el layout */}
+            <MarkerClusterGroup chunkedLoading>
+                 {
+                    animalitos.map((animalito) => (
+                       /** Marcador individual por cada animalito */
+                        <Marker
+                            key={animalito.id}
+                            position={[animalito.latitudAprox,animalito.longitudAprox]}
+                            icon={iconoAnimalito(animalito.especie) || iconoGeneral}
+                        >
+                            <Popup>
+                                <div className="popup-animalito">
+                                    <div className="popup-header popup-animalito-header">{animalito.nombre.toUpperCase()}</div>
+                                    <button className="popup-animalito-button" onClick={() => animalitoHandler(animalito.id) }>Ver tarjeta</button>
+                                </div>
+                            </Popup>
+                        </Marker>
+                    ))
+                }
+            </MarkerClusterGroup>
+
+        </MapContainer>
+    );
+}
+
+export default MapaLeaflet;
+
--- src/paginas/MapaAnimalitos.jsx
+++ src/paginas/MapaAnimalitos.jsx
@@ -1,138 +1,60 @@
 import { useState, useEffect } from "react";
-import { FaMapPin, FaHorse } from "react-icons/fa6";
-import { PiCatFill } from "react-icons/pi";
-import { FaDog } from "react-icons/fa";
-import { SiGooglemaps } from "react-icons/si";
-import L from "leaflet";
-import { renderToStaticMarkup } from "react-dom/server";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import Navbar from "../componentes/Navbar";
+import MapaLeaflet from "../componentes/mapa/MapaLeaflet";
 import { useNavigate } from "react-router-dom";
-import { ubicacion } from "../api/servicioUsuario";
-import { obtenerAnimalitosCercanos } from "../api/servicioAnimalito";
-import MarkerClusterGroup from "react-leaflet-cluster";
+import { me, logout, ubicacion } from "../api/servicioUsuario";
+import { obtenerAnimalitos } from "../api/servicioAnimalito";
 import "leaflet/dist/leaflet.css";
-import "../estilos/paginas/MapaAnimalitos.css"
+import "../estilos/componentes/MapaLeaflet.css";
+import "../estilos/paginas/MapaAnimalitos.css";
 
-const iconoGeneral = L.divIcon({
-    html: renderToStaticMarkup(
-        <SiGooglemaps size={34} color="#BF0413" />
-    ),
-    className: "",
-    iconSize: [32, 32],
-    iconAnchor: [16, 32]
-});
-
-const iconoUsuario = L.divIcon({
-    html: renderToStaticMarkup(
-        <FaMapPin size={38} color="#BF0413" />
-    ),
-    className: "",
-    iconSize: [32, 32],
-    iconAnchor: [16, 32]
-});
-
-const iconoAnimalito = (especie) => {
-    const configuracionBase = {
-        className: "",
-        iconSize: [32, 32],
-        iconAnchor: [16, 32]
-    };
-
-    switch (especie.toUpperCase()) {
-        case "PERRO":
-            return L.divIcon({
-                ...configuracionBase,
-                html: renderToStaticMarkup(
-                    <FaDog size={34} color="#2D3F54" />
-                ),
-            });
-
-        case "GATO":
-            return L.divIcon({
-                ...configuracionBase,
-                html: renderToStaticMarkup(
-                    <PiCatFill size={34} color="#2D3F54" />
-                ),
-            });
-
-        default:
-            return L.divIcon({
-                ...configuracionBase,
-                html: renderToStaticMarkup(
-                    <FaHorse size={34} color="#2D3F54" />
-                )
-            });
-    }
-}
 
 function MapaAnimalitos() {
-
+    const navigate = useNavigate();
+    const [usuario, setUsuario] = useState(null);
     const [posicion, setPosicion] = useState(null);
     const [animalitos, setAnimalitos] = useState([]);
-    const [cargando, setCargando] = useState(true);
-    const navigate = useNavigate();
+    const [cargando,setCargando] = useState(true);
 
     useEffect(() => {
 
         const cargarDatos = async () => {
             try {
+                const usuarioActual = await me();
+                setUsuario(usuarioActual);
+
                 const ubicacionUsuario = await ubicacion();
+                setPosicion([ubicacionUsuario.lat,ubicacionUsuario.lng]);
 
-                setPosicion([ubicacionUsuario.lat, ubicacionUsuario.lng]);
-
-                const animalitosCercanos = await obtenerAnimalitosCercanos();
+                const animalitosCercanos = await obtenerAnimalitos();
                 setAnimalitos(animalitosCercanos);
-
-            } catch (error) {
+            } catch(errror) {
                 console.log(error);
-            } finally {
-                setCargando(false);
             }
-        };
+        }
 
         cargarDatos();
-
     }, [])
 
-    if (cargando || !posicion) {
-        return <div className="mapa-estado">Cargando mapa...</div>;
+    const logoutHandler = async () => {
+        await logout()
+        navigate('/login')
     }
-
 
     const animalitoHandler = (id) => {
         navigate(`/mascota/${id}`);
     }
 
+    if(!posicion) return <div>Cargando mapa...</div>;
+    
     return (
-        <div className="pagina-mapa">
-            <MapContainer className="mapa" center={posicion} zoom={15}>
-                <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <>
+            <Navbar usuario={usuario} onLogout={logoutHandler} />
 
-                <Marker position={posicion} icon={iconoUsuario}>
-                    <Popup>
-                        <div className="popup-usuario">
-                            <div className="popup-header popup-usuario-header"> ¡Aquí estás tú! <FaMapPin/></div>
-                        </div>
-                    </Popup>
-                </Marker>
-                <MarkerClusterGroup chunkedLoading>
-                    {
-                        animalitos.map((animalito) => (
-                            <Marker icon={iconoAnimalito(animalito.especie.toUpperCase().trim()) || iconoGeneral}
-                                key={animalito.id} position={[animalito.latitudAprox, animalito.longitudAprox]}>
-                                <Popup>
-                                    <div className="popup-animalito">
-                                        <div className="popup-header popup-animalito-header">{animalito.nombre.toUpperCase()}</div>
-                                        <button className="popup-animalito-button" onClick={()=>animalitoHandler(animalito.id)}>Ver tarjeta</button>
-                                    </div>
-                                </Popup>
-                            </Marker>
-                        ))
-                    }
-                </MarkerClusterGroup>
-
-            </MapContainer>
-        </div>
+            <div className="mapa-animalitos">
+                <MapaLeaflet posicion={posicion} animalitos={animalitos} animalitoHandler={animalitoHandler} />
+            </div>
+        </>
     );
 }
 
